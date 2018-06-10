824289ffa23518ab2e3f361a73caed4eac2077be
trishank96 <mukherjeetrish005@gmail.com>
2018-06-10 14:11:51 +0530

    Changes saved

--- app/src/main/java/com/example/trishmuk/organizationprofile/MainActivity.kt
+++ app/src/main/java/com/example/trishmuk/organizationprofile/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.trishmuk.organizationprofile
 
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bit = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val round = RoundedBitmapDrawableFactory.create(resources,bit)
+        round.cornerRadius = 20f
+        OrgLogo.setImageDrawable(round)
     }
 }
